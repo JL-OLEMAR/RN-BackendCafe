21f8268b3c885eaffac1fd36ba76834991fbdde3
JL-OLEMAR <joseluis19963@gmail.com>
2021-08-03 19:51:50 -0500

    first commit

---
+++ helpers/generar-jwt.js
@@ -0,0 +1,23 @@
+/* eslint-disable prefer-promise-reject-errors */
+const jwt = require('jsonwebtoken')
+
+const generarJWT = (uid = '') => {
+  return new Promise((resolve, reject) => {
+    const payload = { uid }
+
+    jwt.sign(payload, process.env.SECRETORPRIVATEKEY, {
+      expiresIn: '7d'
+    }, (err, token) => {
+      if (err) {
+        console.log(err)
+        reject('No se pudo generar el token')
+      } else {
+        resolve(token)
+      }
+    })
+  })
+}
+
+module.exports = {
+  generarJWT
+}
